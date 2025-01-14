f8ca69f241f2aaa7f3919dd897f6422400717d38
Michael Wilson <michaelwilson777@gmail.com>
2025-01-14 14:48:44 -0500

    started to add navigation to the app

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,24 +1,28 @@
+import { useState } from "react";
 import { FlatList } from "react-native";
 import { Avatar, ListItem } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
 
-const DirectoryScreen = (props) => {
+const DirectoryScreen = ({navigation}) => {
+  const [campsites, setCampsites] = useState(CAMPSITES);
+
   const renderDirectoryItem = ({ item: campsite }) => {
     return (
-      <ListItem>
-          <Avatar source={campsite.image} rounded />
-          <ListItem.Content>
-              <ListItem.Title>{campsite.name}</ListItem.Title>
-              <ListItem.Subtitle>
-                  {campsite.description}
-              </ListItem.Subtitle>
-          </ListItem.Content>
+      <ListItem
+        onPress={() => navigation.navigate('CampsiteInfo', {campsite})}
+      >
+        <Avatar source={campsite.image} rounded />
+        <ListItem.Content>
+          <ListItem.Title>{campsite.name}</ListItem.Title>
+          <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
+        </ListItem.Content>
       </ListItem>
-  );
+    );
   };
 
   return (
     <FlatList
-      data={props.campsites}
+      data={campsites}
       renderItem={renderDirectoryItem}
       keyExtractor={(item) => item.id.toString()}
     />
